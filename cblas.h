839f2d0c77641200bcbf53baa859e30c7735d831
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-08-28 17:55:18 -0700

    start adding fns

--- cblas.h
+++ cblas.h
@@ -1,5 +1,23 @@
 #ifndef __CBLAS_H
 #define __CBLAS_H
 
+#include <stddef.h>
+
+typedef size_t CBLAS_INDEX;
+
+// Level 1
+void cblas_sswap(CBLAS_INDEX n, float *x, CBLAS_INDEX incx, float *y, CBLAS_INDEX incy);
+void cblas_dswap(CBLAS_INDEX n, double *x, CBLAS_INDEX incx, double *y, CBLAS_INDEX incy);
+
+void cblas_sscal(CBLAS_INDEX n, const float a, float *y, CBLAS_INDEX incx);
+void cblas_dscal(CBLAS_INDEX n, const double a, double *y, CBLAS_INDEX incx);
+
+// Level 2
+
+// Level 3
+
+// Utility
+void cblas_set_num_threads(int threads);
+int cblas_get_num_threads(void);
 
 #endif
