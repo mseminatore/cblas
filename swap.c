735afeaf5234d19aa00e093d1ef9a7a5364fe0e0
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-09-04 11:01:31 -0700

    checkpoint

--- swap.c
+++ swap.c
@@ -1,13 +1,26 @@
 #include "cblas.h"
 
 //
+//
+//
 void cblas_sswap(CBLAS_INDEX n, float *x, CBLAS_INDEX incx, float *y, CBLAS_INDEX incy)
 {
+    if (!x || !y)
+        return;
 
+    float temp;
+    for (CBLAS_INDEX i = 0; i < n; i++)
+    {
+        
+    }
 }
 
 //
+//
+//
 void cblas_dswap(CBLAS_INDEX n, double *x, CBLAS_INDEX incx, double *y, CBLAS_INDEX incy)
 {
+    if (!x || !y)
+        return;
     
 }
