2782d4be8d422198d2ccd07b662665de54f31af3
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-10-14 14:37:17 -0700

    add term colors

--- test.h
+++ test.h
@@ -6,6 +6,21 @@
 
 #include "assert.h"
 
+// screen control
+#define ESC "\x1b"
+#define TERM_CLEAR ESC "[2J" ESC " [H"
+
+// text colors
+#define TERM_RESET ESC "[0m"
+#define TERM_BLACK ESC "[30m"
+#define TERM_RED ESC "[31m"
+#define TERM_GREEN ESC "[32m"
+#define TERM_YELLOW ESC "[33m"
+#define TERM_BLUE ESC "[34m"
+#define TERM_MAGENTA ESC "[35m"
+#define TERM_CYAN ESC "[36m"
+#define TERM_WHITE ESC "[37m"
+
 #ifdef _WIN32
 #   define CHECK_MARK "\xFB"
 #else
@@ -14,9 +29,9 @@
 
 // simple test harness
 #define BEGIN_TESTS()   puts("Beginning test cases...")
-#define TEST(s)         printf("\t%d checking that: " #s " ", ++test_number); assert(s); printf(CHECK_MARK); putchar('\n')
+#define TEST(s)         printf("\t%d checking that: " #s " ", ++test_number); assert(s); printf(TERM_GREEN CHECK_MARK TERM_RESET); putchar('\n')
 #define SUITE(s)        puts("\nTesting suite " s "...\n"); test_suites++
-#define END_TESTS()     printf("\n...end test cases.\nSuccessfully completed %d tests in %d suites.\n\n", test_number, test_suites)
+#define END_TESTS()     printf("\n...finished test cases.\nSuccessfully completed %s%d%s tests in %s%d%s suites.\n\n", TERM_GREEN, test_number, TERM_RESET, TERM_GREEN, test_suites, TERM_RESET)
 
 #ifndef TRUE
 #	define TRUE 1
@@ -30,11 +45,6 @@
 
 #define EQUAL_ARRAY(a, b)       !memcmp((a), (b), sizeof(a))
 #define NOT_EQUAL_ARRAY(a, b)   !EQUAL_ARRAY(a, b)
-
-// void test_string();
-// void test_stdlib();
-// void test_ctype();
-// void test_stdio();
 
 extern int test_number;
 
