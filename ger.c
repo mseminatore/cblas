aa9571b85e34ae47f599947e3a79f4e0f9925b62
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-10-28 17:51:48 -0700

    fix build break

--- ger.c
+++ ger.c
@@ -11,26 +11,26 @@
 //------------------------------------------------------
 void cblas_sger(CBLAS_LAYOUT layout, CBLAS_INDEX m, CBLAS_INDEX n, float alpha, float *x, CBLAS_INDEX incx, float *y, CBLAS_INDEX incy, float *a, CBLAS_INDEX lda)
 {
-	if (alpha == 1.0f)
-	{
-		for (int a_col = 0; a_col < a->cols; a_col++)
-		{
-			for (int b_col = 0; b_col < b->cols; b_col++)
-			{
-				dest->values[a_col * b->cols + b_col] += a->values[a_col] * b->values[b_col];
-			}
-		}
-	}
-	else
-	{
-		for (int a_col = 0; a_col < a->cols; a_col++)
-		{
-			for (int b_col = 0; b_col < b->cols; b_col++)
-			{
-				dest->values[a_col * b->cols + b_col] += alpha * a->values[a_col] * b->values[b_col];
-			}
-		}
-	}
+	// if (alpha == 1.0f)
+	// {
+	// 	for (int a_col = 0; a_col < a->cols; a_col++)
+	// 	{
+	// 		for (int b_col = 0; b_col < b->cols; b_col++)
+	// 		{
+	// 			dest->values[a_col * b->cols + b_col] += a->values[a_col] * b->values[b_col];
+	// 		}
+	// 	}
+	// }
+	// else
+	// {
+	// 	for (int a_col = 0; a_col < a->cols; a_col++)
+	// 	{
+	// 		for (int b_col = 0; b_col < b->cols; b_col++)
+	// 		{
+	// 			dest->values[a_col * b->cols + b_col] += alpha * a->values[a_col] * b->values[b_col];
+	// 		}
+	// 	}
+	// }
 }
 
 //------------------------------------------------------
