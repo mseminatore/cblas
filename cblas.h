35927bb24b50c2e33fa29bc250112cd925a51063
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-11-04 14:35:37 -0700

    tidy up

--- cblas.h
+++ cblas.h
@@ -1,9 +1,17 @@
+//------------------------------------------------------
+//
+// Copyright 2023 Mark Seminatore. All rights reserved.
+//------------------------------------------------------
+
 #ifndef __CBLAS_H
 #define __CBLAS_H
 
 #include <stddef.h>
 #include <math.h>
 
+//------------------------------------------------------
+//
+//------------------------------------------------------
 typedef size_t CBLAS_INDEX;
 
 typedef int CBLAS_LAYOUT;
@@ -13,7 +21,9 @@ enum {
     CblasColMajor
 };
 
+//------------------------------------------------------
 // Level 1 functions
+//------------------------------------------------------
 float cblas_sdot(CBLAS_INDEX n, float *x, CBLAS_INDEX incx, const float *y, CBLAS_INDEX incy);
 double cblas_ddot(CBLAS_INDEX n, double *x, CBLAS_INDEX incx, const double *y, CBLAS_INDEX incy);
 
@@ -41,18 +51,26 @@ double cblas_dasum(CBLAS_INDEX n, double *x, CBLAS_INDEX incx);
 float cblas_snrm2(CBLAS_INDEX n, float *x, CBLAS_INDEX incx);
 double cblas_dnrm2(CBLAS_INDEX n, double *x, CBLAS_INDEX incx);
 
+//------------------------------------------------------
 // Level 2 functions
+//------------------------------------------------------
 void cblas_sger(CBLAS_LAYOUT layout, CBLAS_INDEX m, CBLAS_INDEX n, float alpha, float *x, CBLAS_INDEX incx, float *y, CBLAS_INDEX incy, float *a, CBLAS_INDEX lda);
 void cblas_dger(CBLAS_LAYOUT layout, CBLAS_INDEX m, CBLAS_INDEX n, double alpha, double *x, CBLAS_INDEX incx, double *y, CBLAS_INDEX incy, double *a, CBLAS_INDEX lda);
 
 void cblas_sgemv();
 void cblas_dgemv();
 
+//------------------------------------------------------
 // Level 3 functions
+//------------------------------------------------------
 void cblas_sgemm();
 void cblas_dgemm();
 
+//------------------------------------------------------
 // Utility functions
+//------------------------------------------------------
+void cblas_init();
+void cblas_shutdown();
 void cblas_set_num_threads(int threads);
 int cblas_get_num_threads(void);
 
