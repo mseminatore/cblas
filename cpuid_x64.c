2a73d65311b5ada9beedb5bea13a958ae86305b0
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-11-21 15:15:44 -0800

    add missing file cpuid_x64.c

---
+++ cpuid_x64.c
@@ -0,0 +1,17 @@
+//------------------------------------------------------
+//
+// Copyright 2023 Mark Seminatore. All rights reserved.
+//------------------------------------------------------
+#include <stdio.h>
+
+//
+const char *cpu_get_core_name()
+{
+    return "Generic x64";
+}
+
+//
+int cpu_get_core_count()
+{
+    return 1;
+}
