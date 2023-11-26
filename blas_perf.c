ba53b5851e843e53e8c5063cce098fca4801903c
Mark Seminatore <mseminatore@users.noreply.github.com>
2023-11-25 18:01:38 -0800

    start of perf eval

--- blas_perf.c
+++ blas_perf.c
@@ -20,5 +20,21 @@ int main(int argc, char *argv[])
     printf("      CPU uArch: %s\n", cblas_get_corename());
     printf("  Cores/Threads: %d/%d\n", cblas_get_num_procs(), cblas_get_num_threads());
 	
+    struct timespec t1, t2;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t1);
+
+    float x[1024], y[1024], a[1024*1024];
+
+    cblas_sger(CblasRowMajor, 1024, 1024, 1.0f, x, 1, y, 1, a, 1024);
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t2);
+
+    int seconds = (int)(t2.tv_sec - t1.tv_sec);
+    long long ns = t2.tv_nsec - t1.tv_nsec;
+    float dt = (float)seconds + (float)ns/(1000000000);
+
+    printf("%f GLOPS in %fs\n", (float)2 * 0.001048f / dt, dt);
+
 	return 0;
 }
