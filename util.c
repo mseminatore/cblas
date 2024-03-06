00e0097f11516eada942da7febab101df87ffb58
Mark Seminatore <mseminatore@users.noreply.github.com>
2024-03-06 13:08:44 -0800

    add start of mem test

--- util.c
+++ util.c
@@ -154,7 +154,40 @@ void cblas_level1_exec(CBLAS_INDEX stride, kernel_function kernel, CBLAS_INDEX n
 //------------------------------------------------------
 void cblas_level2_exec()
 {
+    work_queue_t queue[MAX_THREADS];
+    cblas_args_t args[MAX_THREADS];
 
+    int thread_count = CLAMP(cblas_get_num_threads(), 1, MAX_THREADS);
+
+    //for (int i = 0; i < thread_count; i++)
+    //{
+    //    args[i].incx = incx;
+    //    args[i].incy = incy;
+
+    //    // compute partition starts based on remaining task size and remaining threads
+    //    CBLAS_INDEX partition_size = (n + thread_count - i - 1) / (thread_count - i);
+
+    //    args[i].n = partition_size;
+    //    args[i].x = x;
+    //    args[i].y = y;
+
+    //    n -= partition_size;
+
+    //    // TODO - the x/y is wrong when incx/incy is > 1
+    //    x = (void*)((CBLAS_INDEX)x + partition_size * incx * stride);
+    //    y = (void*)((CBLAS_INDEX)y + partition_size * incy * stride);
+
+    //    queue[i].finished = 0;
+    //    queue[i].args = &args[i];
+    //    queue[i].kernel = kernel;
+    //    queue[i].next = &queue[i + 1];
+    //}
+
+    //// mark end of task queue
+    //queue[thread_count - 1].next = NULL;
+
+    //// synchronously execute task queue
+    //cblas_execute(thread_count, queue);
 }
 
 //------------------------------------------------------
