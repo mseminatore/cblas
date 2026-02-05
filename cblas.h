962d4b8a2a9236d0e7c9da11c29d8ca2e8e08be0
Mark Seminatore <mseminatore@users.noreply.github.com>
2026-02-04 19:34:02 -0800

    add swap kernel fns

--- cblas.h
+++ cblas.h
@@ -512,6 +512,10 @@ typedef struct
 	kernel_function scopy_k_noinc;  // single-precision copy inc=1
     kernel_function dcopy_k;        // double-precision copy
 	kernel_function dcopy_k_noinc;  // double-precision copy inc=1
+    kernel_function sswap_k;        // single-precision swap
+    kernel_function dswap_k;        // double-precision swap
+    kernel_function sswap_k_noinc;  // single-precision swap inc=1
+    kernel_function dswap_k_noinc;  // double-precision swap inc=1
 
     kernel_function sscal_k;        // single-precision scale
     kernel_function dscal_k;        // double-precision scale
