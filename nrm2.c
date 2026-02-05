e79dbda992cd6b9dd0854ecb0dd57ed22e63c11b
Mark Seminatore <mseminatore@users.noreply.github.com>
2026-02-04 23:14:43 -0800

    enable MT for nrm2

--- nrm2.c
+++ nrm2.c
@@ -15,7 +15,11 @@ float cblas_snrm2(CBLAS_INDEX n, float *x, CBLAS_INDEX incx)
     CBLAS_VALIDATE_VEC1(n, x, incx, sum);
     CBLAS_STATS_START();
 
+#if defined(MT_ENABLED)
+    int mt_used = (n > CBLAS_MT_DOT) ? 1 : 0;
+#else
     int mt_used = 0;
+#endif
 
     kernel_function kernel = blas_kernels.snrm2_k;
 
@@ -25,13 +29,29 @@ float cblas_snrm2(CBLAS_INDEX n, float *x, CBLAS_INDEX incx)
         kernel = blas_kernels.snrm2_k_noinc;
     }
 
-    cblas_args_t args;
-    args.n = n;
-    args.x = x;
-    args.incx = incx;
-    args.c = &sum;
+    if (mt_used)
+    {
+        float thread_partial_sums[MAX_THREADS];
 
-    kernel(&args);
+        cblas_level1_exec_result(sizeof(float), kernel, n, x, incx, NULL, 0, thread_partial_sums, "SNRM2");
+
+        // accumulate partial sums of squares
+        CBLAS_INDEX threads = cblas_get_num_threads();
+        for (CBLAS_INDEX i = 0; i < threads; i++)
+        {
+            sum += thread_partial_sums[i];
+        }
+    }
+    else
+    {
+        cblas_args_t args;
+        args.n = n;
+        args.x = x;
+        args.incx = incx;
+        args.c = &sum;
+
+        kernel(&args);
+    }
 
     CBLAS_STATS_END("snrm2", n, mt_used);
 
@@ -48,7 +68,11 @@ double cblas_dnrm2(CBLAS_INDEX n, double *x, CBLAS_INDEX incx)
     CBLAS_VALIDATE_VEC1(n, x, incx, sum);
     CBLAS_STATS_START();
 
+#if defined(MT_ENABLED)
+    int mt_used = (n > CBLAS_MT_DOT) ? 1 : 0;
+#else
     int mt_used = 0;
+#endif
 
     kernel_function kernel = blas_kernels.dnrm2_k;
 
@@ -58,13 +82,29 @@ double cblas_dnrm2(CBLAS_INDEX n, double *x, CBLAS_INDEX incx)
         kernel = blas_kernels.dnrm2_k_noinc;
     }
 
-    cblas_args_t args;
-    args.n = n;
-    args.x = x;
-    args.incx = incx;
-    args.c = &sum;
+    if (mt_used)
+    {
+        double thread_partial_sums[MAX_THREADS];
 
-    kernel(&args);
+        cblas_level1_exec_result(sizeof(double), kernel, n, x, incx, NULL, 0, thread_partial_sums, "DNRM2");
+
+        // accumulate partial sums of squares
+        CBLAS_INDEX threads = cblas_get_num_threads();
+        for (CBLAS_INDEX i = 0; i < threads; i++)
+        {
+            sum += thread_partial_sums[i];
+        }
+    }
+    else
+    {
+        cblas_args_t args;
+        args.n = n;
+        args.x = x;
+        args.incx = incx;
+        args.c = &sum;
+
+        kernel(&args);
+    }
 
     CBLAS_STATS_END("dnrm2", n, mt_used);
 
