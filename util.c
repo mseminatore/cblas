a4877090a97532721a8c0faed5addaa5b23d7067
Mark Seminatore <mseminatore@users.noreply.github.com>
2026-06-17 22:13:24 -0700

    fix active thread reporting

--- util.c
+++ util.c
@@ -532,6 +532,7 @@ void cblas_print_configuration(void)
         printf(" Cores/Threads: %d/%d\n", cpu_get_core_count(), cpu_get_logical_core_count());
     }
     
+    printf("Active threads: %d\n", cblas_get_num_threads());
     printf(" L1$ line size: %d bytes\n", cpu_get_cacheline_size());
     printf(" L1$ data size: %d Kbytes\n", cpu_get_l1_data_cache_size());
     printf("      L2$ size: %d Kbytes\n", cpu_get_l2_cache_size());
